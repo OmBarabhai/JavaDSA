e50e5ba67d126042ee0fe56aceae221bb15a707c
OmBarabhai <om.r.barabhai@gmail.com>
2026-04-01 23:26:22 +0530

    add Basics

---
+++ src/Basics_Java/DataType.java
@@ -0,0 +1,15 @@
+package Basics_Java;
+
+public class DataType {
+    static void main(){
+        // Numeric Data Type - short, byte, int, long
+        byte num1 = 127;//128 + give error loss of data
+        System.out.println(num1);
+        short num2 = 32767;
+        System.out.println(num2);
+        int num3 =  949879472;
+        long num4 = 9498794721l;
+        float num5 = 3.14f;
+
+    }
+}
